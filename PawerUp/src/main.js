7db2dae1ee20413d3579a066c7faa4b5c942cc08
TangRuotong <tina1319980173@gmail.com>
2026-01-20 09:27:30 +0800

    Clean up main.js by removing unused Firebase code
    Removed commented-out Firebase configuration and initialization code.

--- PawerUp/src/main.js
+++ PawerUp/src/main.js
@@ -14,21 +14,3 @@ app.use(PrimeVue, { theme: { preset: Aura } })
 app.use(router)
 
 app.mount('#app')
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from 'firebase/app'
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyCmfS_VaBN7Nz9MXlJHCwCxVeZipo4XxkA',
-//   authDomain: 'pawerup-92f54.firebaseapp.com',
-//   projectId: 'pawerup-92f54',
-//   storageBucket: 'pawerup-92f54.firebasestorage.app',
-//   messagingSenderId: '117236558919',
-//   appId: '1:117236558919:web:3e764f6ff99231608f9562',
-// }
-
-// // Initialize Firebase
-// initializeApp(firebaseConfig)
